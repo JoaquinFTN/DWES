2e879c909b56a527b2a45035c524307a523aa6a0
JoaquinFTN <147615627+JoaquinFTN@users.noreply.github.com>
2024-10-14 12:16:58 +0200

    Update principal.php

--- proObjetos/tareas/TAR-06.02/principal.php
+++ proObjetos/tareas/TAR-06.02/principal.php
@@ -2,6 +2,5 @@
     require_once 'clase.php';
     $objClase = new Fechas();
     $objClase->getFecha($_GET["fecha"]);
-    $fecha=$objClase->generarFecha();
-    echo $fecha;
+    echo $fecha=$objClase->generarFecha();
 ?>
